995e27a87fcc555702f4a4228b1e16173063793e
Matias Belvedere <matiasbelvedere@hotmail.com>
2022-10-06 10:49:55 -0300

    sithxth commit

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const mainContainerQuestion = document.querySelectorAll(".mainContainerQuestion");
-const mainContainerQuestionText = document.querySelectorAll(".mainContainerQuestionText")
+const mainContainerQuestionText = document.querySelectorAll(".mainContainerQuestionText");
 const buttonDown = document.querySelectorAll(".buttonDown");
 const mainContainerAnswer = document.querySelectorAll(".mainContainerAnswer");
-let mainContainerAnswerText = document.querySelectorAll(".mainContainerAnswerText")
-const imageBox = document.querySelector(".imageBox")
-
-const mainContainerFAQ = document.querySelectorAll(".mainContainerFAQ")
+let mainContainerAnswerText = document.querySelectorAll(".mainContainerAnswerText");
+const imageBox = document.querySelector(".imageBox");
 
 for (let i = 0; i < mainContainerAnswer.length; i++) {
     let heightContainer = mainContainerAnswerText[i].clientHeight;
@@ -23,22 +21,6 @@ for (let i = 0; i < mainContainerAnswer.length; i++) {
 
 
     mainContainerQuestion[i].addEventListener("click", () => {
-        mainContainerAnswer.forEach(element => {
-            element.style.height = `0px`;
-        });
-        
-        mainContainerQuestionText.forEach (element => {
-            element.style.fontWeight = "400";
-            element.style.color = "var(--veryDarkGraBlue)";
-            element.addEventListener("mouseout", () => {
-                element.style.color = ("var(--veryDarkGraBlue)");
-            });
-        });
-
-        buttonDown.forEach (e => {
-            e.style.rotate = "0deg";
-        });
-        
 
         if (change == 0) {
             mainContainerAnswer[i].style.height = `${heightContainer}px`;
@@ -49,10 +31,9 @@ for (let i = 0; i < mainContainerAnswer.length; i++) {
             mainContainerQuestionText[i].addEventListener("mouseout", () => {
                 mainContainerQuestionText[i].style.color = ("black")
             })
+
             change--;
-        } 
-        
-        else {
+        } else {
             mainContainerAnswer[i].style.height = `0px`;
             mainContainerQuestionText[i].style.fontWeight = "400";
             mainContainerQuestionText[i].style.color = "var(--veryDarkGraBlue)";
@@ -61,9 +42,8 @@ for (let i = 0; i < mainContainerAnswer.length; i++) {
             mainContainerQuestionText[i].addEventListener("mouseout", () => {
                 mainContainerQuestionText[i].style.color = ("var(--veryDarkGraBlue)")
             })
+
             change++;   
         }
     })
-    
-    
 }
